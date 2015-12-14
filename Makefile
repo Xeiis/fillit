e512afc205ae33d959c638597ea3f4a6c9a0e6d0
Ludwig DUBOS <ldubos@e2r9p12.42.fr>
2015-12-14 16:59:22 +0100

    Adding fillit.h and some files ...

--- Makefile
+++ Makefile
@@ -5,63 +5,9 @@
 #                                                     +:+ +:+         +:+      #
 #    By: ldubos <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/11/27 16:26:37 by ldubos            #+#    #+#              #
-#    Updated: 2015/12/14 16:04:28 by dchristo         ###   ########.fr        #
+#    Created: 2015/12/07 13:19:55 by ldubos            #+#    #+#              #
+#    Updated: 2015/12/07 13:28:40 by ldubos           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = fillit
 
-CC = gcc
-
-FLAGS = -Wall -Werror -Wextra
-
-SRC = $(shell ls -1 | grep "\.c")
-
-OBJS = $(SRC:.c=.o)
-
-LIB = libft/libft.a
-
-all: $(NAME) 
-
-$(NAME): 
-	@git clone vogsphere@vogsphere.42.fr:intra/2015/activities/libft/dchristo2 libft
-	@cd libft ; $(MAKE) -f Makefile
-	@$(CC) $(FLAGS) -c $(SRC)
-	@$(CC) $(FLAGS) -o $(NAME) $(OBJS) $(LIB)
-	@echo "+---------------------------------------------+";
-	@echo "|   ______ _____ _      _      _____ _______  |";
-	@echo "|  |  ____|_   _| |    | |    |_   _|__   __| |";
-	@echo "|  | |__    | | | |    | |      | |    | |    |";
-	@echo "|  |  __|   | | | |    | |      | |    | |    |";
-	@echo "|  | |     _| |_| |____| |____ _| |_   | |    |";
-	@echo "|  |_|    |_____|______|______|_____|  |_|    |";
-	@echo "|                                             |";
-	@echo "+---------------------------------------------+";
-	
-%.o : %.c
-	$(CC) $(FLAGS) $< -o $@
-
-clean:	
-	rm -rf $(OBJS)
-	@cd libft ; $(PHONY make clean)
-
-fclean:	clean
-	rm -f $(NAME)
-	@cd libft ; $(PHONY make fclean)	
-	rm -rf libft
-	
-re:	fclean all write
-
-write:
-	@echo "+---------------------------------------------+";
-	@echo "|   ______ _____ _      _      _____ _______  |";
-	@echo "|  |  ____|_   _| |    | |    |_   _|__   __| |";
-	@echo "|  | |__    | | | |    | |      | |    | |    |";
-	@echo "|  |  __|   | | | |    | |      | |    | |    |";
-	@echo "|  | |     _| |_| |____| |____ _| |_   | |    |";
-	@echo "|  |_|    |_____|______|______|_____|  |_|    |";
-	@echo "|                                             |";
-	@echo "+---------------------------------------------+";
-
-.PHONY: all clean fclean re write
---
+++ main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldubos <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/07 13:18:39 by ldubos            #+#    #+#             */
+/*   Updated: 2015/12/10 20:28:14 by ldubos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+#define DEFAULT   "\033[0m"
+#define HIGHLIGHT "\033[1m"
+#define UNDERLINE "\033[4m"
+#define BLINK     "\033[5m"
+#define BLACK     "\033[30m"
+#define RED       "\033[31m"
+#define GREEN     "\033[32m"
+#define YELLOW    "\033[33m"
+#define BLUE      "\033[34m"
+#define PURPLE    "\033[35m"
+#define CYAN      "\033[36m"
+#define WHITE     "\033[37m"
+
+double				ft_sqrt(double n, double i);
+
+int					main(int argc, char **argv)
+{
+	size_t			i;
+
+	i = 1;
+	printf(HIGHLIGHT PURPLE "\t\t\t/* ************************************************************************** */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/*                                                                            */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/*                                                                            */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/*   Created: 2015/12/07 13:18:39 by ldubos                                   */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/*                                                                            */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/*                                                                            */\n");
+	printf(HIGHLIGHT PURPLE "\t\t\t/* ************************************************************************** */\n" DEFAULT);
+	printf("Square(s) => %d\n\n", argc - 1);
+	if (argc < 2)
+		printf(HIGHLIGHT RED "/!\\ Too few arguments /!\\" DEFAULT);
+	else
+		while (i < argc)
+		{
+			printf("[Square(" CYAN "%lu" DEFAULT  ")]{nbr of tetrimino(s) = %i}\t\t=>\t\tMin square size[" CYAN "%zu x %zu" DEFAULT "]\n", i, ft_atoi(argv[i]), (size_t)(ft_sqrt((ft_atoi(argv[i]) * 4), 0) + 0.5), (size_t)(ft_sqrt((ft_atoi(argv[i]) * 4), 0) + 0.5));
+			++i;
+		}
+	return (0);
+}
