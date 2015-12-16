66f21543d2ff7a036b0cabed5c51342eac974e41
Ludwig DUBOS <ldubos@e2r9p9.42.fr>
2015-12-16 17:09:24 +0100

    Removing printf

--- ft_gettab.c
+++ ft_gettab.c
@@ -6,7 +6,7 @@
 /*   By: ldubos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 18:36:14 by ldubos            #+#    #+#             */
-/*   Updated: 2015/12/16 15:46:29 by dchristo         ###   ########.fr       */
+/*   Updated: 2015/12/16 17:08:52 by ldubos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,8 @@ static	int					ft_test(t_tetrimino tetrimino)
 	i = 0;
 	while (i < 4)
 	{
-		printf(" [%d]x = %d, [%d]x =%d\n",i,tetrimino.c_pos[i].x,i+1,tetrimino.c_pos[i+1].x);
-		printf(" [%d]y = %d, [%d]y =%d\n",i,tetrimino.c_pos[i].y,i+1,tetrimino.c_pos[i+1].y);
 		x_d = tetrimino.c_pos[i].x - tetrimino.c_pos[i + 1].x;
 		y_d = tetrimino.c_pos[i].y - tetrimino.c_pos[i + 1].y;
-		printf("x_d = %d",x_d);
-		printf("y_d = %d",y_d);
 		if ((x_d >= -1 && x_d <= 1 && y_d == 0) ||
 			(y_d >= -1 && y_d <= 1 && x_d == 0))
 			++i;
