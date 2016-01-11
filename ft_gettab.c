d06d672bba63caf8c83ac84b8a715422938c312a
Damien CHRISTOPHE <dchristo@e2r2p10.42.fr>
2016-01-11 18:50:46 +0100

    verfication de la derniere ligne ok cette fois si

--- ft_gettab.c
+++ ft_gettab.c
@@ -6,7 +6,7 @@
 /*   By: ldubos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 18:36:14 by ldubos            #+#    #+#             */
-/*   Updated: 2016/01/11 18:34:15 by dchristo         ###   ########.fr       */
+/*   Updated: 2016/01/11 18:50:14 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,20 @@ static	int					ft_test(t_tetrimino tetrimino, int i)
 	return (1);
 }
 
-static	void				ft_init(int *x, int *y, int *i, int *boucle)
+static	void				ft_init(int *x, int *y, int *i)
 {
 	*x = 0;
 	*y = 0;
 	*i = 0;
-	*boucle = 0;
 }
 
-static	int					ft_statement(char *str, t_tetrimino *tetrimino)
+static	int					ft_statement(char *str, t_tetrimino *tetrimino, int b)
 {
 	int						x;
 	int						y;
 	int						i;
-	int						b;
 
-	ft_init(&x, &y, &i, &b);
+	ft_init(&x, &y, &i);
 	while (*++str && i < 5 && (*str == '#' || *str == '.' || *str == '\n'))
 	{
 		if (*str == '\n')
@@ -72,21 +70,21 @@ static	int					ft_statement(char *str, t_tetrimino *tetrimino)
 			tetrimino->c_pos[i++].y = y;
 		}
 		(*str == '.' || *str == '#') ? x++ : x;
-		b++;
 	}
-	if (i > 4 || ((b != 21 && *--str != '\0') || (b != 20 && *str != '\n')))
+	if (i > 4 || ((b == 21 && *--str != '\n') || (b == 20 && *str != '\0')))
 		return (0);
 	return (1);
 }
 
-static int					ft_read(int fd, int i, t_tetrimino **ret)
+static int					ft_read(int fd, int i, t_tetrimino **ret, int b)
 {
 	t_tetrimino				*t;
 	char					*buf;
+	int						b_save;
 
 	buf = ft_strnew(BUF_S);
 	t = *ret;
-	while (read(fd, buf, BUF_S) != 0)
+	while ((b = read(fd, buf, BUF_S)) != 0)
 	{
 		if (i != 0)
 		{
@@ -96,14 +94,16 @@ static int					ft_read(int fd, int i, t_tetrimino **ret)
 		}
 		t->w_pos.x = 0;
 		t->w_pos.y = 0;
-		if (!(ft_statement(--buf, t)))
+		if (!(ft_statement(--buf, t, b)))
 			return (0);
-		t->c = 'A' + i;
+		t->c = 'A' + i++;
 		if (!(ft_test(*t, 0)))
 			return (0);
 		ft_bzero(buf, BUF_S + 1);
-		++i;
+		b_save = b;
 	}
+	if (b_save != 20)
+		return (0);
 	t->next = NULL;
 	return (1);
 }
@@ -114,5 +114,5 @@ int							ft_gettab(const char *path, t_tetrimino **t)
 	
 	if ((fd = open(path, O_RDONLY)) == -1)
 		return (0);
-	return (ft_read(fd, 0, t));
+	return (ft_read(fd, 0, t, 0));
 }
